ac5e8b3a0eb3582642ffee0833980fddf7b74342
zhangyunan <zyndev@gmail.com>
2022-02-08 14:37:16 +0800

    增加仓库地址

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,11 @@
 module.exports = {
+    base: '/bug-wheels/',
+
     // 站点配置
     lang: 'zh-CN',
     title: '你好， VuePress ！',
     description: '这是我的第一个 VuePress 站点',
-  
+    repo: 'bug-wheels/bug-wheels',
     // 主题和它的配置
     theme: '@vuepress/theme-default',
     themeConfig: {
